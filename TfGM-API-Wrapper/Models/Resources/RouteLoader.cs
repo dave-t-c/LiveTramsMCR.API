a5d3f16d9d87e24b8330562530df645a45eb5738
David Cook <davidthomascook@live.co.uk>
2022-10-01 14:09:50 +0100

    feat: Add expected number of routes to imported routes using UnprocessedRoutes.

--- TfGM-API-Wrapper/Models/Resources/RouteLoader.cs
+++ TfGM-API-Wrapper/Models/Resources/RouteLoader.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 using TfGM_API_Wrapper.Models.RoutePlanner;
 using TfGM_API_Wrapper.Models.Stops;
 
@@ -9,19 +11,34 @@ namespace TfGM_API_Wrapper.Models.Resources;
 /// </summary>
 public class RouteLoader
 {
+    private ResourcesConfig _resourcesConfig;
+    private List<Stop> _importedStops;
+    
+    /// <summary>
+    /// Loads routes from the resources configuration and assigns the imported
+    /// stops to those on a route.
+    /// </summary>
+    /// <param name="resourcesConfig">Configuration location of routes file</param>
+    /// <param name="importedStops">Imported Stops to assign to Routes</param>
     public RouteLoader(ResourcesConfig resourcesConfig, List<Stop> importedStops)
     {
-        
+        _resourcesConfig = resourcesConfig;
+        _importedStops = importedStops;
     }
 
+    /// <summary>
+    /// Imports routes from constructor resource configuration, and
+    /// adds the relevant stop instances to the routes.
+    /// </summary>
+    /// <returns>List of Imported Routes</returns>
     public List<Route> ImportRoutes()
     {
-        List<Route> importedRoutes = new List<Route>();
-        for (int i = 0; i < 8; i++)
-        {
+        using var reader = new StreamReader(_resourcesConfig.RoutesResourcePath);
+        var jsonString = reader.ReadToEnd();
+        var unprocessedRoutes = JsonConvert.DeserializeObject<List<UnprocessedRoute>> (jsonString);
+        var importedRoutes = new List<Route>();
+        for(int i = 0; i < unprocessedRoutes?.Count; i++)
             importedRoutes.Add(new Route("", "", new List<Stop>()));
-        }
-
         return importedRoutes;
     }
 }
