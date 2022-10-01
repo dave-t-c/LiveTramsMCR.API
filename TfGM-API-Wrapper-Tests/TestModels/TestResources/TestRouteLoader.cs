6b4cc63a34a837b48c3bf6a35a6e062c63af59fc
David Cook <davidthomascook@live.co.uk>
2022-10-01 16:16:08 +0100

    test: Add test to import an expected route and check it contains a stop.

--- TfGM-API-Wrapper-Tests/TestModels/TestResources/TestRouteLoader.cs
+++ TfGM-API-Wrapper-Tests/TestModels/TestResources/TestRouteLoader.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using TfGM_API_Wrapper.Models.Resources;
 using TfGM_API_Wrapper.Models.Stops;
@@ -12,11 +13,11 @@ namespace TfGM_API_Wrapper_Tests.TestModels.TestResources;
 /// </summary>
 public class TestRouteLoader
 {
-    private const string StopResourcePathConst = "../../../Resources/ValidStopLoader.json";
     private const string StationNamesToTlarefsPath = "../../../Resources/Station_Names_to_TLAREFs.json";
     private const string TlarefsToIdsPath = "../../../Resources/TLAREFs_to_IDs.json";
     private const string RoutesResourcePath = "../../../Resources/TestRoutePlanner/routes.json";
     private const string RoutesDiffLengthResourcePath = "../../../Resources/TestRoutePlanner/routes-diff-length.json";
+    private const string StopResourcePathConst = "../../../Resources/TestRoutePlanner/stops.json";
     private ResourcesConfig? _validResourcesConfig;
     private ResourcesConfig? _diffRoutesResourcesConfig;
     private StopLoader? _stopLoader;
@@ -76,5 +77,24 @@ public class TestRouteLoader
         Assert.NotNull(importedRoutes);
         Assert.IsNotEmpty(importedRoutes);
         Assert.AreEqual(7, importedRoutes.Count);
+    }
+
+    /// <summary>
+    /// Test to see if the Purple route contains the
+    /// Piccadilly stop.
+    /// This should be the last stop on the list, and should only exist once.
+    /// </summary>
+    [Test]
+    public void TestRouteContainsImportedStops()
+    {
+        var testRouteLoader = new RouteLoader(_validResourcesConfig, _importedStops);
+        var importedRoutes = testRouteLoader.ImportRoutes();
+        Assert.NotNull(importedRoutes);
+        Assert.IsNotEmpty(importedRoutes);
+        Assert.AreEqual(8, importedRoutes.Count);
+        var piccadillyStop = _importedStops?.First(stop => stop.StopName == "Piccadilly");
+        var purpleRoute = importedRoutes.First(route => route.Name == "Purple");
+        Assert.Contains(piccadillyStop, purpleRoute.Stops);
+        Assert.AreEqual(1, purpleRoute.Stops.FindAll(stop => stop.StopName == "Piccadilly").Count);
     }
 }
