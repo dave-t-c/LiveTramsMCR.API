fcaa8d75bbe058819c114700045a9866f947e8fa
David Cook <davidthomascook@live.co.uk>
2022-10-04 18:34:22 +0100

    feat: Add null check to origin in Interchange required method.

--- TfGM-API-Wrapper/Models/RoutePlanner/RouteIdentifier.cs
+++ TfGM-API-Wrapper/Models/RoutePlanner/RouteIdentifier.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using TfGM_API_Wrapper.Models.Stops;
 
@@ -22,6 +23,9 @@ public class RouteIdentifier
     /// <returns>If there is route that contains both Stops</returns>
     public bool IsInterchangeRequired(Stop origin, Stop destination)
     {
+        if (origin is null)
+            throw new ArgumentNullException(nameof(origin));
+        
         //Returns true if there is a route that contains both origin and dest stops.
         //Find returns null if there is not a match, so interchange is required if there is not a match
         return _routes.Find(route => route.Stops.Contains(origin) && route.Stops.Contains(destination)) is null;
