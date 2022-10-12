8f412a42e818a3dbeb2cc7c9d6ff60b4da50e779
David Cook <davidthomascook@live.co.uk>
2022-10-12 18:00:11 +0100

    feat: Create basic controller for journey planning.

---
+++ TfGM-API-Wrapper/Controllers/JourneyPlannerController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TfGM_API_Wrapper.Models.RoutePlanner;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace TfGM_API_Wrapper.Controllers;
+
+/// <summary>
+/// Controller for handling requests related to journey planning.
+/// </summary>
+[Route("/api/journey-planner")]
+[ApiController]
+public class JourneyPlannerController: Controller
+{
+
+    private readonly IJourneyPlannerModel _journeyPlannerModel;
+    
+    /// <summary>
+    /// Creates a new JourneyPlannerController and uses the given IJourneyPlannerModel
+    /// to process requests.
+    /// </summary>
+    /// <param name="journeyPlannerModel"></param>
+    public JourneyPlannerController(IJourneyPlannerModel journeyPlannerModel)
+    {
+        _journeyPlannerModel = journeyPlannerModel;
+    }
+
+    /// <summary>
+    /// Plans a journey between an origin and destination stop.
+    /// </summary>
+    /// <param name="origin">Origin stop name or tlaref</param>
+    /// <param name="destination">Destination stop name or tlaref</param>
+    /// <returns>A planned journey between the origin an destination</returns>
+    [Route("/api/journey-planner/{origin}/{destination}")]
+    [Produces("application/json")]
+    [SwaggerResponse (type:typeof (PlannedJourney), statusCode: StatusCodes.Status200OK)]
+    [HttpGet]
+    public IActionResult PlanJourney(string origin, string destination)
+    {
+        return Ok(_journeyPlannerModel.PlanJourney(origin, destination));
+    }
+}
