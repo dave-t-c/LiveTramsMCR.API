4ffe2e1a1e4d62c7b1e0ae578ca9b8eb9d7d09d3
David Cook <davidthomascook@live.co.uk>
2023-04-10 13:02:05 +0100

    fix: Remove un-used interfaces from stop.

--- LiveTramsMCR/Models/V1/Stops/Stop.cs
+++ LiveTramsMCR/Models/V1/Stops/Stop.cs
@@ -12,7 +12,7 @@ namespace LiveTramsMCR.Models.V1.Stops;
 /// </summary>
 // ReSharper disable once ClassNeverInstantiated.Global
 // ReSharper disable UnusedMember.Global
-public sealed class Stop : IEquatable<Stop>, IEqualityComparer<Stop>
+public sealed class Stop
 {
     /// <summary>
     /// Object Id used internally by mongodb.
@@ -112,22 +112,7 @@ public sealed class Stop : IEquatable<Stop>, IEqualityComparer<Stop>
         hashCode.Add(StopName);
         return hashCode.ToHashCode();
     }
-
-    /// <summary>
-    /// Compares two stop objects
-    /// </summary>
-    /// <param name="x"></param>
-    /// <param name="y"></param>
-    /// <returns></returns>
-    public bool Equals(Stop x, Stop y)
-    {
-        if (ReferenceEquals(x, y)) return true;
-        if (ReferenceEquals(x, null)) return false;
-        if (ReferenceEquals(y, null)) return false;
-        if (x.GetType() != y.GetType()) return false;
-        return x.StopName == y.StopName && x.Tlaref == y.Tlaref;
-    }
-
+    
     /// <summary>
     /// Generates a hash code for a stop obj
     /// </summary>
