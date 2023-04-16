b1d6d39214b886b1304573c313cc6eee399b2d42
David Cook <davidthomascook@live.co.uk>
2023-04-16 17:14:14 +0100

    Add basics for stops v2.
    This will pull from the stops v2 collection.

---
+++ LiveTramsMCR/Models/V2/RoutePlanner/RouteV2.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using LiveTramsMCR.Models.V2.Stops;
+using MongoDB.Bson;
+
+namespace LiveTramsMCR.Models.V2.RoutePlanner;
+
+/// <summary>
+/// Class that represents a tram route between two Stops.
+/// The stops are included as the Stop class, so all relevant information is available.
+/// </summary>
+public class RouteV2
+{
+    /// <summary>
+    /// Object ID used by mongodb
+    /// </summary>
+    [JsonIgnore] 
+    public ObjectId Id { get; set; }
+    
+    /// <summary>
+    /// Name of the route, e.g. "Purple"
+    /// </summary>
+    public string Name { get; internal set; }
+    
+    /// <summary>
+    /// Hex colour string for the route, e.g. #7B2082
+    /// </summary>
+    public string Colour { get; internal set; }
+    
+    /// <summary>
+    /// Stops belonging to a route in the order they can be travelled between.
+    /// </summary>
+    public List<StopKeysV2> Stops { get; internal set; }
+    
+    /// <summary>
+    /// Creates a new route, the params are only null sanitised
+    /// </summary>
+    /// <param name="name">Route name, e.g. Purple</param>
+    /// <param name="colour">Route hex colour, e.g. #7B2082</param>
+    /// <param name="stops"></param>
+    ///
+    public RouteV2(string name, string colour, List<StopKeysV2> stops)
+    {
+        Name = name ?? throw new ArgumentNullException(nameof(name));
+        Colour = colour ?? throw new ArgumentNullException(nameof(colour));
+        Stops = stops ?? throw new ArgumentNullException(nameof(stops));
+    }
+
+    /// <summary>
+    /// Identifies a list of stops that occur between two stops
+    /// E.g. for the route A -> B -> C -> D, stops between A and D 
+    /// this will return B then C.
+    /// </summary>
+    /// <param name="start">Stop in list to start</param>
+    /// <param name="end">Stop in list to end at</param>
+    /// <returns>List of Interim Stops</returns>
+    public List<StopKeysV2> GetStopsBetween(StopKeysV2 start, StopKeysV2 end)
+    {
+        _ = start ?? throw new ArgumentNullException(nameof(start));
+        _ = end ?? throw new ArgumentNullException(nameof(end));
+        if (!Stops.Contains(start))
+            throw new InvalidOperationException("Route does not contain stop " + start.StopName);
+        if (!Stops.Contains(end))
+            throw new InvalidOperationException("Route does not contain stop " + end.StopName);
+        var startIndex = Stops.IndexOf(start);
+        var endIndex = Stops.IndexOf(end);
+        // Use the differences in index to identify if we will be going up or down the list.
+        var increment = endIndex > startIndex ? 1 : -1;
+        var identifiedStops = new List<StopKeysV2>();
+        for (int i = startIndex + increment; i != endIndex; i += increment)
+        {
+            identifiedStops.Add(Stops.ElementAt(i));
+        }
+        return identifiedStops;
+    }
+
+    /// <summary>
+    /// Determines if this route contains a given Stop.
+    /// </summary>
+    /// <param name="stop">Stop to check membership of</param>
+    /// <returns>True if route contains Stop, else false.</returns>
+    public bool ContainsStop(StopKeysV2 stop)
+    {
+        return Stops.Contains(stop);
+    }
+}
---
+++ LiveTramsMCR/Models/V2/Stops/Data/StopsRepository.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+
+namespace LiveTramsMCR.Models.V2.Stops.Data;
+
+/// <inheritdoc />
+public class StopsRepositoryV2 : IStopsRepositoryV2
+{
+    private readonly IMongoCollection<StopV2> _stopsCollection;
+    
+    /// <summary>
+    /// Create a new stops repository using a stops collection
+    /// </summary>
+    /// <param name="stopsCollection"></param>
+    public StopsRepositoryV2(IMongoCollection<StopV2> stopsCollection)
+    {
+        _stopsCollection = stopsCollection;
+    }
+    
+    /// <inheritdoc />
+    public StopV2 GetStop(string searchTerm)
+    {
+        return _stopsCollection.FindAsync(stop =>
+                stop.StopName.Equals(searchTerm, StringComparison.OrdinalIgnoreCase)
+                || stop.Tlaref.Equals(searchTerm, StringComparison.OrdinalIgnoreCase)
+                ).Result.FirstOrDefault();
+    }
+
+    /// <inheritdoc />
+    public List<StopV2> GetAll()
+    {
+        return _stopsCollection.FindAsync(_ => true).Result.ToList();
+    }
+}
