c44a4c9afd16688df7bee422f335697ed5b7a35b
David Cook <davidthomascook@live.co.uk>
2023-05-28 22:29:25 +0100

    func: Create basic RouteV2 class.
    This is currently a POCO.

---
+++ LiveTramsMCR/Models/V2/RoutePlanner/RouteV2.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using LiveTramsMCR.Models.V2.Stops;
+using MongoDB.Bson;
+
+namespace LiveTramsMCR.Models.V2.RoutePlanner;
+
+/// <summary>
+/// Represents route information. 
+/// </summary>
+public class RouteV2 {
+
+    /// <summary>
+    /// Object ID used by mongodb
+    /// </summary>
+    [JsonIgnore] 
+    public ObjectId Id { get; set; }
+
+    /// <summary>
+    /// Name of the route, e.g. "Purple"
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// Hex colour string for the route, e.g. #7B2082
+    /// </summary>
+    public string Colour { get; set; }
+
+    /// <summary>
+    /// Stops belonging to a route in the order they can be travelled between.
+    /// </summary>
+    public List<StopKeysV2> Stops { get; set; }
+
+    /// <summary>
+    /// List of co-ordinates to create a polyline for the route.
+    /// This follows the same direction as the stops.
+    /// </summary>
+    public List<List<double>> PolylineCoordinates { get; set; }
+}
