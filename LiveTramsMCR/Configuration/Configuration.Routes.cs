a710a6a8a8c6836cc1b54307549f601f782ce202
David Cook <davidthomascook@live.co.uk>
2023-10-06 23:51:21 +0100

    Improve closest stop identification range.
    This decreaes the tolerance to the same resolution as the route / stop location lat / longs.

--- LiveTramsMCR/Configuration/Configuration.Routes.cs
+++ LiveTramsMCR/Configuration/Configuration.Routes.cs
@@ -5,5 +5,5 @@ internal static partial class Configuration
     /// <summary>
     /// Location tolerance used when comparing coordinates.
     /// </summary>
-    internal static double LocationAccuracyTolerance => 0.000001;
+    internal static double LocationAccuracyTolerance => 0.00000000001;
 }
