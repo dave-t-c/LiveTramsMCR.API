dad2bef0cfa3cac9cdad0be66474a93b8edd328b
David Cook <45901991+dave-t-c@users.noreply.github.com>
2024-03-17 17:10:42 +0000

    Use ubuntu instead of windows.

--- LiveTramsMCR.Tests/Configuration/TestAppConfiguration.cs
+++ LiveTramsMCR.Tests/Configuration/TestAppConfiguration.cs
@@ -4,5 +4,5 @@ internal static class TestAppConfiguration
 {
     internal static double RouteCoordinateTolerance => 0.000001;
 
-    internal static string TestDbConnectionString => "mongodb://foo:bar@localhost:27017?authSource=admin";
+    internal static string TestDbConnectionString => "mongodb://foo:bar@localhost:27017";
 }
