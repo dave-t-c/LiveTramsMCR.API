140ae496815d1221d885765c168bd730ada8f73c
David Cook <45901991+dave-t-c@users.noreply.github.com>
2024-04-23 00:13:41 +0100

    Update test config to run test in container.

--- LiveTramsMCR.Tests/Configuration/TestAppConfiguration.cs
+++ LiveTramsMCR.Tests/Configuration/TestAppConfiguration.cs
@@ -4,7 +4,7 @@ internal static class TestAppConfiguration
 {
     internal static double RouteCoordinateTolerance => 0.000001;
 
-    internal static string TestDbConnectionString => "mongodb://foo:bar@localhost:27017";
+    internal static string TestDbConnectionString => "mongodb://foo:bar@mongo:27017";
 
     internal static string LocalDynamoDbServiceUrl => "http://localhost:8000";
 }
