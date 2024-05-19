b1391de04ef0bcee852acd894d8a6c234b16e10c
David Cook <45901991+dave-t-c@users.noreply.github.com>
2024-05-19 21:49:45 +0100

    Use lambda entry point for api gateway rest api.
    This will allow cache to be configured as it is not supported for http apis.

--- LiveTramsMCR/LambdaEntryPoint.cs
+++ LiveTramsMCR/LambdaEntryPoint.cs
@@ -5,7 +5,7 @@ namespace LiveTramsMCR;
 /// <summary>
 /// Class used as a entry point for Lambda function
 /// </summary>
-public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
+public class LambdaEntryPoint : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
 {
     /// <inheritdoc />
     protected override void Init(IWebHostBuilder builder)
